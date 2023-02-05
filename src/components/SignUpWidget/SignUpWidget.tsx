7cbd6bf20e87eb0fe916d172499cc67de3140351
alex-gudkov-innowise <alex.gudkov@innowise-group.com>
2023-02-06 02:09:37 +0300

    Add sign up modal window

--- src/components/SignUpWidget/SignUpWidget.tsx
+++ src/components/SignUpWidget/SignUpWidget.tsx
@@ -1,9 +1,14 @@
 import { Button } from '@material-ui/core';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import React from 'react';
+import React, { useState } from 'react';
+
 import './SignUpWidget.css';
+import SignUpModal from '../SignUpModal/SignUpModal';
 
 function SignUpWidget() {
+    const [isVisibleSignUpModal, setVisibleSignUpModal] = useState(false);
+    const [isVisibleLogInWindow, setVisibleLogInWindow] = useState(false);
+
     return (
         <div className="SignUpWidget">
             <div className="SignUpWidget__header">
@@ -12,8 +17,31 @@ function SignUpWidget() {
             </div>
             <div className="SignUpWidget__body">
                 <div className="SignUpWidget__body__text">Join Twitter today!</div>
-                <Button className="SignUpWidget__body__signUp">Sign Up</Button>
-                <Button className="SignUpWidget__body__signIn">Log in</Button>
+
+                <Button
+                    variant="outlined"
+                    className="SignUpWidget__body__signUp"
+                    fullWidth
+                    onClick={() => {
+                        setVisibleSignUpModal(true);
+                    }}
+                >
+                    Sign Up
+                </Button>
+                <Button
+                    variant="outlined"
+                    className="SignUpWidget__body__signIn"
+                    fullWidth
+                    onClick={() => {
+                        setVisibleLogInWindow(true);
+                    }}
+                >
+                    Log in
+                </Button>
+
+                <SignUpModal visible={isVisibleSignUpModal} setVisible={setVisibleSignUpModal} />
+
+                {/* <LogInWindow /> */}
             </div>
         </div>
     );
