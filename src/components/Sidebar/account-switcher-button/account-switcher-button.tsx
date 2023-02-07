e2b8729613b0ac4056283d45b083b6061bc4eee0
alex-gudkov-innowise <alex.gudkov@innowise-group.com>
2023-02-07 15:05:46 +0300

    Use BEM style naming for account switcher button

--- src/components/Sidebar/account-switcher-button/account-switcher-button.tsx
+++ src/components/Sidebar/account-switcher-button/account-switcher-button.tsx
@@ -1,22 +1,24 @@
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import React from 'react';
+
 import './account-switcher-button.css';
+import { user } from '../../../server-response/user';
 
 function AccountSwitcherButton() {
     return (
-        <div className="Sidebar__account">
-            <div className="Sidebar__account__container">
+        <div className="AccountSwitcherButton">
+            <div className="AccountSwitcherButton__body">
                 <img
                     src={require('../../../static/images/avatar.png')}
                     alt="avatar"
-                    className="Sidebar__account__avatar"
+                    className="AccountSwitcherButton__user-avatar"
                 />
-                <div className="Sidebar__account__body">
-                    <span className="Sidebar__account__body__name">Alex</span>
-                    <span className="Sidebar__account__body__nick">@alex423342</span>
+                <div className="AccountSwitcherButton__column">
+                    <span className="AccountSwitcherButton__user-name">{user.name}</span>
+                    <span className="AccountSwitcherButton__user-nick">@{user.nick}</span>
                 </div>
             </div>
-            <MoreHorizIcon className="Sidebar__account__more" />
+            <MoreHorizIcon className="AccountSwitcherButton__more" />
         </div>
     );
 }
