14ea69726e1536f5010e55e5f15e735ad6cb25d8
alex-gudkov-innowise <alex.gudkov@innowise-group.com>
2023-02-09 19:33:19 +0300

    Add navigate hook on profile header back button

--- src/components/profile/profile-header/profile-header.tsx
+++ src/components/profile/profile-header/profile-header.tsx
@@ -1,13 +1,21 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import './profile-header.css';
 import { user } from '../../../server-response/user';
 
+import './profile-header.css';
+
 function ProfileHeader() {
+    const navigate = useNavigate();
+
+    function navigateToHome() {
+        navigate('/home');
+    }
+
     return (
         <div className="ProfileHeader">
-            <ArrowBackIcon className="ProfileHeader__arrow-back" />
+            <ArrowBackIcon className="ProfileHeader__back-button" onClick={navigateToHome} />
             <div className="ProfileHeader__body">
                 <h1 className="ProfileHeader__user-name">{user.name}</h1>
                 <span className="ProfileHeader__tweets-count">17 Tweets</span>
