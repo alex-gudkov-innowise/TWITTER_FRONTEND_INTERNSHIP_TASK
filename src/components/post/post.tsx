1f6ab7e78d186c5548d0506b41d17abd3da0eddf
alex-gudkov-innowise <alex.gudkov@innowise-group.com>
2023-02-15 22:21:39 +0300

    Add separated page for viewing the record

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -1,19 +1,22 @@
+import './post.css';
 import React from 'react';
 
+import { useNavigateTo } from '../../hooks/use-navigate-to';
 import { RecordsEntity } from '../../interfaces/records-entity';
 
 import PostBody from './post-body/post-body';
 import PostFooter from './post-footer/post-footer';
 import PostHeader from './post-header/post-header';
-import './post.css';
 
 interface PostProps {
     record: RecordsEntity;
 }
 
 function Post({ record }: PostProps) {
+    const navigateToRecordId = useNavigateTo('/record/' + record.id);
+
     return (
-        <div className="Post">
+        <div className="Post" onClick={navigateToRecordId}>
             <PostHeader record={record} />
             <PostBody record={record} />
             <PostFooter />
---
+++ src/components/record-page/record-page.tsx
@@ -0,0 +1,43 @@
+import './record-page.css';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useFetching } from '../../hooks/use-fetch';
+import { useNavigateTo } from '../../hooks/use-navigate-to';
+import { RecordsEntity } from '../../interfaces/records-entity';
+import { RecordsService } from '../../services/records-service';
+import Post from '../post/post';
+import Sidebar from '../sidebar/sidebar';
+import Widgets from '../widgets/widgets';
+
+function RecordPage() {
+    const { recordId = '' } = useParams<string>();
+    const [record, setRecord] = useState<RecordsEntity>();
+    const [fetchRecord, isRecordLoading] = useFetching(async () => {
+        const record = await RecordsService.getRecordById(recordId);
+
+        setRecord(record);
+    });
+    const navigateToHome = useNavigateTo('/home');
+
+    useEffect(() => {
+        fetchRecord();
+    }, []);
+
+    return (
+        <div className="RecordPage">
+            <Sidebar activeElement="RecordPage" />
+            <div className="RecordPage__container">
+                <div className="RecordPage__header">
+                    <ArrowBackIcon className="RecordPage__back-button" onClick={navigateToHome} />
+                    <h1 className="RecordPage__title">Tweet</h1>
+                </div>
+                {record ? <Post record={record} /> : ''}
+            </div>
+            <Widgets />
+        </div>
+    );
+}
+
+export default RecordPage;
